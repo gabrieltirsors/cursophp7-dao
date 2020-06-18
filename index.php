877e7d749a4ca301c94f72eccc96f409da1f284b
gabrieltirsors <gabriel.tirso@gabrieltsantos.com.br>
2020-06-18 00:02:07 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega 1 usuario
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista dos usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "qwerwr");
+echo $usuario;
 
  ?>
